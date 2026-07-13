aa766d8624ebda4073fa7101e4f2e3d09015af97
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-13 18:17:25 +0530

    arraylist updated

--- collection/src/arrayList.java
+++ collection/src/arrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class arrayList {
@@ -8,7 +9,7 @@ public class arrayList {
 //        students[0] = "anushka";
 //        students[1] = "kushal";
 
-        List<String> List = new ArrayList();
+        List<String> List = new ArrayList<>();
         //adding elements
         List.add("anuska");
         System.out.println(List);
@@ -33,6 +34,42 @@ public class arrayList {
         List.addAll(newlist);
         System.out.println(List);
 
+        //get element of particular index
+        System.out.println(newlist.get(1));
 
+        //removing elements
+        //by index:
+        List.remove(1);
+        System.out.println(List);
+        //by element value:
+        List.remove(String.valueOf("anuska"));
+        System.out.println(List);
+        //remove whole list
+        newlist.clear();
+        System.out.println(newlist);
+
+        //updating the values
+        List.set(0,"anushka");
+        System.out.println(List);
+
+        //element present or not
+        System.out.println(List.contains("tanu"));
+
+        //ITERATION
+        // 1: for loop
+        for (int i=0; i<List.size(); i++){
+            System.out.println("the elements of list are :" +List.get(i));
+        }
+
+        // 2:foreach loop
+        for (String value : List){
+            System.out.println("ELEMENTS ARE : " +value);
+
+            // 3:ITERATOR
+            Iterator<String> it = List.iterator();
+            while(it.hasNext()){
+                System.out.println("elements through iterator are : " +it.next());
+            }
+        }
     }
 }
